9c44abcc994d94b778cb380fdb5fef59616916a4
hanthuhein14 <wke9818@gmail.com>
2026-08-15 22:29:54 +0630

    fix backend

--- backend/app/config.py
+++ backend/app/config.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     database_password:str
     database_name:str
     database_username:str
+    database_sslmode:str = "prefer"
     secret_key:str
     algorithm:str
     access_token_expire_minutes:int
--- backend/app/database.py
+++ backend/app/database.py
@@ -8,7 +8,10 @@ from .config import settings
 SQLALCHEMY_DATABASE_URL=f'postgresql+psycopg://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
 
-engine=create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"sslmode": settings.database_sslmode}
+)
 
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
