b9279aaa77c27f240f5c92511cda0e8829915b83
hanthuhein14 <wke9818@gmail.com>
2026-08-16 17:50:40 +0630

    database connection fixed

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,11 +1,19 @@
-from sqlalchemy import create_engine, engine_from_config
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from .config import settings
 
 
-SQLALCHEMY_DATABASE_URL=f'postgresql+psycopg://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+SQLALCHEMY_DATABASE_URL = URL.create(
+    drivername="postgresql+psycopg",
+    username=settings.database_username,
+    password=settings.database_password,
+    host=settings.database_hostname,
+    port=int(settings.database_port),
+    database=settings.database_name,
+)
 
 
 engine = create_engine(
